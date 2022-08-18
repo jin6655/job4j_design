35af26be3a04154c807fd5be4bba4f316c57955d
jin6655 <vasilypoiser@yandex.ru>
2022-08-18 21:33:41 +0300

    6. Перевернуть связанный список

--- src/main/java/ru/job4j/collection/SimpleQueue.java
+++ src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -5,27 +5,18 @@ public class SimpleQueue<T> {
     private final SimpleStack<T> in = new SimpleStack<>();
     private final SimpleStack<T> out = new SimpleStack<>();
 
-    private int countIn;
-    private int countOut;
-
     public T poll() {
-        while (countIn > 0) {
+        while (in.size > 0) {
             out.push(in.pop());
-            countIn--;
-            countOut++;
         }
-        countOut--;
         return out.pop();
     }
 
     public void push(T value) {
-        while (countOut > 0) {
+        while (out.size > 0) {
             in.push(out.pop());
-            countIn++;
-            countOut--;
         }
         in.push(value);
-        countIn++;
     }
 
 }
--- src/main/java/ru/job4j/collection/SimpleStack.java
+++ src/main/java/ru/job4j/collection/SimpleStack.java
@@ -3,14 +3,17 @@ package ru.job4j.collection;
 public class SimpleStack<T> {
 
     private ForwardLinked<T> linked = new ForwardLinked<T>();
+    public int size;
 
     // Метод pop() - должен возвращать значение и удалять его из коллекции.
     public  T pop() {
+        size--;
         return linked.deleteFirst();
     }
 
     //Метод push(T value) - помещает значение в коллекцию.
     public void push(T value) {
+        size++;
         linked.addFirst(value);
     }
 

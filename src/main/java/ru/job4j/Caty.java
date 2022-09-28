7d20547096a87175d591222a239616d41bb85a71
jin6655 <vasilypoiser@yandex.ru>
2022-09-28 21:42:07 +0300

    2. Simple Loggin Facade 4 Java.

--- src/main/java/ru/job4j/Caty.java
+++ src/main/java/ru/job4j/Caty.java
@@ -1,20 +1,5 @@
 package ru.job4j;
 
-import com.sun.source.tree.Tree;
-import ru.job4j.io.PrintFiles;
-import ru.job4j.io.duplicates.FileProperty;
-import ru.job4j.list.SimpleArrayList;
-import ru.job4j.map.SimpleMap;
-import ru.job4j.set.Set;
-import ru.job4j.set.SimpleSet;
-
-import java.io.*;
-import java.nio.file.*;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.util.*;
-
-import static java.nio.file.FileVisitResult.CONTINUE;
-
 public class Caty {
 
     private int x;
@@ -42,10 +27,8 @@ public class Caty {
                 + '}';
     }
 
-    public static void main(String[] args) throws IOException {
-        try (FileWriter out = new FileWriter("./src/main/resources/log4j.properties")) {
-            out.write("");
-        }
+    public static void main(String[] args) {
+        System.out.println(Caty.class.getName());
     }
 
 }
